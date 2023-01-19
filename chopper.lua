33ad34c95298e89aa6d2f54abed6ebd18a8c9bc4
Felipe <silva.felipe12@hotmail.com>
2023-01-19 14:02:39 -0300

    Update chopper.lua

---
+++ chopper.lua
@@ -0,0 +1,25 @@
+ 1. --[[Turtle program to chopp tree using computercraft inside minecraft.]]
+ 3.
+ 4. if not turtle.detect() then
+ 5.   error('Não encontrei um árvore!')
+ 6. end
+ 7.
+ 8. print('Cortando árvore...')
+ 9.
+10. if not turtle.dig() then  -- corta a base da árvore
+11.   error('Turtle precisa de uma ferramenta para cortar!')
+12. end
+13.
+14. turtle.forward()  -- se move para baixo da árvore
+15. while turtle.compareUp() do
+16.   -- continua cortando até que não tenha mais árvore
+17.   turtle.digUp()
+18.   turtle.up()  
+19. end
+20.
+21. -- Volta para o inicio no chão
+22. while not turtle.detectDown() do
+23.   turtle.down()
+24. end
+25.
+26. print('Árvore cortada, fim!.')
